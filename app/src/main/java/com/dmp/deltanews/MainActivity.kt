f80cf02798e5b2e0d236c3779e87054493d08f4b
developer-dmp <developer.dmp@gmail.com>
2021-08-24 23:22:19 -0400

    Initial commit - project creation!

---
+++ app/src/main/java/com/dmp/deltanews/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.dmp.deltanews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val database = Firebase.database
+        val myRef = database.getReference("message")
+
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                Log.i("SNAPSHOT", snapshot.value.toString())
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Nothing to do
+            }
+        })
+    }
+}
